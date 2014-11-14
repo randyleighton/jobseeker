c5ff4514dae65d2fcec2efc6ac80abd79e99e3c7
Randy Leighton <randyleighton@yahoo.com>
2014-11-14 15:28:14 -0800

    update contacts and companies view to fix error

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,9 +12,9 @@ class CompaniesController < ApplicationController
 	def create
 		@company = Company.create(company_params)
 		if @company.valid?
-      		redirect_to companies_path, notice: "#{@company.name} saved"
+      		redirect_to companies_path, notice: "#{@company.name} created."
 	    else
-	      	flash[:alert] = "#{@company.name} failed to save"
+	      	flash[:alert] = "Company could not be created."
 	      	render 'new'
 	    end
 	end
@@ -24,7 +24,12 @@ class CompaniesController < ApplicationController
 	end
 
 	def update
-
+		@company = @company.find([:id])
+		if @company.update_attributes(company_params).valid?
+			redirect_to :back, notice: "#{@company.name} updated."
+		else
+			render company_path(@company), alert: "Failed to Update."
+		end
 	end
 
 	def destroy
