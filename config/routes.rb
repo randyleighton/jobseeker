45299cedf8b525e2537cd3ab792270e38a17e62c
Randy Leighton <randyleighton@yahoo.com>
2014-11-16 13:33:45 -0800

    contact all display all contacts, no company needed for index "

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :companies do
       resources :contacts
   end
+  resources :contacts, only: :index
   resources :jobs
   resources :job_applications
   
