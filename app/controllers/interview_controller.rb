b30a8de23ce0ddf31f8ce32f1a8401d41b16f88c
Randy Leighton <randyleighton@yahoo.com>
2014-11-21 19:41:06 -0800

    interview index new create controller

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -1,4 +1,25 @@
 class InterviewsController < ApplicationController
   before_filter :authenticate_user!
 
+def index
+    @all_interviews = Interview.all
+  end
+
+  def new
+    @interview = Interview.new
+  end
+
+  def create
+    @interview = @interview.create(interview_params)
+    if @interview.valid?
+      redirect_to interviews_path, notice: "Interview on #{@interview.date} created."
+    else
+      render 'new', alert: "Interview could not be created."
+    end
+  end
+
+
+end
+
+
 end
