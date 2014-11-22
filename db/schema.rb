ac0f0d7b0f6582ebb127573e053a73c74d5b86e3
Randy Leighton <randyleighton@yahoo.com>
2014-11-21 19:13:38 -0800

    fix jobs to job in that migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102033146) do
+ActiveRecord::Schema.define(version: 20141122031042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20141102033146) do
   end
 
   add_index "contacts", ["company_id"], name: "index_contacts_on_company_id", using: :btree
+
+  create_table "interviews", force: true do |t|
+    t.integer "interview_date"
+    t.string  "notes"
+    t.integer "jobs_id"
+  end
+
+  add_index "interviews", ["jobs_id"], name: "index_interviews_on_jobs_id", using: :btree
 
   create_table "jobs", force: true do |t|
     t.string  "description"
