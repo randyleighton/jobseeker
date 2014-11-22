4bc30e6887b4c83efe62e0f5245931551c442f42
Randy Leighton <randyleighton@yahoo.com>
2014-11-21 19:41:34 -0800

    interview controller edit update show delete

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -18,6 +18,38 @@ def index
     end
   end
 
+  def show
+
+  end
+
+  def edit
+    render 'new'
+  end
+
+  def update
+    if @interview.update_attributes(interview_params)
+      redirect_to interviews_path, notice: "#Interview updated."
+    else
+      render interview_path(@interview), alert: "Failed to Update."
+    end
+  end
+
+  def destroy
+    @interview.destroy
+    flash[:notice]="Interview deleted"
+    redirect_to interviews_path
+  end
+
+  private
+
+  def interview_params
+    params.require(:interview).permit(:interview_date, :notes, :interview_id)
+  end
+
+  def not_found
+      session[:return_to]||= root_url
+      redirect_to session[:return_to], flash: {alert: 'Problem finding record, you might not be authorized.'}
+  end
 
 end
 
