1b6fbb758e34168d678c1c5efa06c75e06cddb12
Randy Leighton <randyleighton@yahoo.com>
2014-11-29 12:05:29 -0800

    one_step should have many contacts through companies relation and test

--- app/models/one_step.rb
+++ app/models/one_step.rb
@@ -1,5 +1,7 @@
 class OneStep < ActiveRecord::Base
 
 has_many :companies
+has_many :contacts, through: :companies
+
 
 end
--- spec/models/one_step_spec.rb
+++ spec/models/one_step_spec.rb
@@ -1,5 +1,6 @@
 describe OneStep do
 
 it { should have_many :companies }
+it { should have_many(:contacts).through(:companies) }
 
 end
