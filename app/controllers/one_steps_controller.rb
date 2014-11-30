7431bdc8934c7322bb5fec9865fc1b733e525951
Randy Leighton <randyleighton@yahoo.com>
2014-11-29 19:33:21 -0800

    one step progress, main form is created but only submitting company at moment

--- app/controllers/one_steps_controller.rb
+++ app/controllers/one_steps_controller.rb
@@ -1,15 +1,13 @@
 class OneStepsController < ApplicationController
 	before_filter :authenticate_user!
 
-
   def index
-    @one_steps = One_steps.all
   end
 
   def new
     @company = Company.new
-    @contact = @company.contact.new
-    @job = @company.job.new
+    @contact = @company.contacts.new
+    @job = @company.jobs.new
   end
 
 end
--- app/models/one_step.rb
+++ app/models/one_step.rb
@@ -1,11 +1,9 @@
-class OneStep < ActiveRecord::Base
+class One_step < ActiveRecord::Base
 
-has_many :companies
-has_many :contacts, through: :companies
-has_many :jobs, through: :companies
+  has_many :companies
+  has_many :contacts, through: :companies
+  has_many :jobs, through: :companies
 
-accepts_nested_attributes_for :companies
-
-
+  accepts_nested_attributes_for :companies
 
 end
