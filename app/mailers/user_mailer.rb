b6e3e4ed0119ddffdb1859e33523caff5ba30c7d
Randy Leighton <randyleighton@yahoo.com>
2014-12-01 18:43:44 -0800

    user confirmation messages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "randyleighton@yahoo.com"
+  default from: "randyleighton@visible-difference.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.signup_confirmation.subject
-  #
   def signup_confirmation(user)
     @user = user
-    mail to: "randyleighton@yahoo.com"
+    @greeting = "Hello"
+    mail to: "randyleighton@yahoo.com", subject: "Sign up for Jobseeker"
   end
+
 end
