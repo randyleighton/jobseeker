2ce16e1e891c7efd3f98c565910237e771bea0e9
Randy Leighton <randyleighton@yahoo.com>
2014-12-01 21:39:02 -0800

    layout and configuration of mail files html and text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ActionMailer::Base
 
   def signup_confirmation(user)
     @user = user
-    @greeting = "Hello"
+    @url = "http://localhost:3000/users/login"
     mail to: "randyleighton@yahoo.com", subject: "Sign up for Jobseeker"
   end
 
   def send_reminder(user, job)
     @user = user
-    @greeting = "Hello"
+    @url = "http://localhost:3000/users/login"
     @job = job
     mail to: "#{@user.email}", subject: "#{@job.description} - Reminder"
   end
