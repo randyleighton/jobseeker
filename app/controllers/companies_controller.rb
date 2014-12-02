6b54333698197aa028529e667dc78c12d11f2879
Randy Leighton <randyleighton@yahoo.com>
2014-12-02 00:09:21 -0800

    company controller flash alert changed to notice for destroy

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -37,7 +37,7 @@ class CompaniesController < ApplicationController
 
 	def destroy
 		@company.destroy
-    flash[:alert]="Company and all its contacts and jobs were deleted."
+    flash[:notice]="Company deleted, including all its contacts and jobs."
    	redirect_to root_path
 	end
 
