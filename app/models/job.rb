ae4e438d7b202436ba0e8242b38559e32d91f3d3
Randy Leighton <randyleighton@yahoo.com>
2014-12-02 10:18:14 -0800

    fix relation job has many responses

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ActiveRecord::Base
 
 belongs_to :company
 has_many :interviews, dependent: :destroy
-has_one :response, dependent: :destroy
+has_many :responses, dependent: :destroy
 
 validates_presence_of :description, :location
 validates :description, uniqueness: true
