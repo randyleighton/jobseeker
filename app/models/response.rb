fffb4d59760efb9a7dec9531c1bf253ca32bdebd
Randy Leighton <randyleighton@yahoo.com>
2014-12-02 12:52:09 -0800

    response validate presence of date + test

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,6 @@ class Response < ActiveRecord::Base
 belongs_to :job
 has_one :company, through: :job
 
+validates :response_date, presence: true
+
 end
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,6 +1,7 @@
 describe Response do
 
   it { should belong_to :job }
-  it { should have_one(:company).through(:job)}
+  it { should have_one(:company).through(:job) }
+  it { should validate_presence_of :response_date }
 
 end
