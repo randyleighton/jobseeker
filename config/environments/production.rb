2b20ab59efb4d2785fb6483897a1b6d058959871
Randy Leighton <randyleighton@yahoo.com>
2014-12-03 11:50:04 -0800

    mail setup params

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,16 +76,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #mailer setup for mailgun
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # :authentication => :plain,
-  # :address => "smtp.mailgun.org",
-  # :port => 587,
-  # :domain => ENV["mailgun_sandbox_domain"],
-  # :user_name => ENV["mailgun_sandbox_username"],
-  # :password => ENV["mailgun_sandbox_password"]
-  # }
+  mailer setup for mailgun
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :authentication => :plain,
+  :address => "smtp.mailgun.org",
+  :port => 587,
+  :domain => ENV["mailgun_sandbox_domain"],
+  :user_name => ENV["mailgun_sandbox_username"],
+  :password => ENV["mailgun_sandbox_password"]
+  }
 
 
 
