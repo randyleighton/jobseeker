e4137a42c432473edc64c00b33be75e59f43f5d0
Randy Leighton <randyleighton@yahoo.com>
2014-12-03 01:35:36 -0800

    remove mailer from model for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  after_create :signup_confirmation
+  # after_create :signup_confirmation
 
   has_many :companies
   has_many :jobs, through: :companies
   has_many :interviews, through: :companies
 
-  def signup_confirmation
-    UserMailer.signup_confirmation(self).deliver
-  end
+  # def signup_confirmation
+  #   UserMailer.signup_confirmation(self).deliver
+  # end
 end
