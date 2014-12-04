0f58aebfdc91a3cf2814ddfc3e515e978b5df901
Randy Leighton <randyleighton@yahoo.com>
2014-12-04 13:44:51 -0800

    MAILER success - From displays like this The Jobseeker <postmaster...@mailgun.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,7 @@ class UserMailer < ActionMailer::Base
   def signup_confirmation(user)
     @user = user
     @email = @user.email
-    @from = ENV['mailgun_sandbox_username']
-    binding.pry
-    mail(to: @email, from: @from,subject: 'Welcome to My Jobseeker')
+    mail(to: @email, subject: 'Welcome to My Jobseeker')
   end
   
 end
