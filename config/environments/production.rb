057f97384c0525605eb7d6fd276b89b6f2d6bd54
Randy Leighton <randyleighton@yahoo.com>
2014-12-03 22:19:19 -0800

    add   config.action_mailer.default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #added for devise forgot password
+  config.action_mailer.default_url_options = {host: "thejobseeker.herokuapp.com"}
+
   # mailer setup for mailgun
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
