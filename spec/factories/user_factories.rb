8c429890f8388a0b79c48ced16d4e5389290605a
Randy Leighton <randyleighton@yahoo.com>
2014-12-06 11:52:18 -0800

    factory girl setup helper

--- spec/factories/user_factories.rb
+++ spec/factories/user_factories.rb
@@ -8,18 +8,31 @@ FactoryGirl.define do
   end
 
   factory :company do
-
+    name Faker::Company.name
+    url Faker::Internet.url
+    comments
   end
 
   factory :contact do
-
+    first_name Faker::Name.first_name
+    last_name Faker::last_name
+    title Faker::title
+    email Faker::Internet.email
+    cell_phone Faker::PhoneNumber.cell_phone
+    work_phone Faker::PhoneNumber.phone_number
   end
 
   factory :job do
+    description "Ruby Rails Developer"
+    location Faker::Address.city
+    posting_url Faker::Internet.url
+    application_date Faker::Date.between(2.days.ago, Date.today)
+    status "open"
 
   end
 
   factory :interview do
+    interview_date Faker::Date.forward(7)
 
   end
 
