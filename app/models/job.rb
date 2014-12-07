d5bf2ff9b3a90d94a6b9712d74f9a20a4ba219d7
Randy Leighton <randyleighton@yahoo.com>
2014-12-06 18:15:14 -0800

    application date can not be a future date - add method and validate call for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,22 +1,32 @@
 class Job < ActiveRecord::Base
-before_save :text_style
+  before_save :text_style
+  
+  belongs_to :company
+  has_many :interviews, dependent: :destroy
+  has_many :responses, dependent: :destroy
+  has_one :user, through: :company
 
-belongs_to :company
-has_many :interviews, dependent: :destroy
-has_many :responses, dependent: :destroy
-has_one :user, through: :company
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :application_date, presence: true
+  validate :verify_date
 
-validates_presence_of :description, :location, :application_date
+  scope :order_by, ->{ order(created_at: :desc)}
 
-scope :order_by, ->{ order(created_at: :desc)}
+  def verify_date
+    if self.application_date > Time.now
+      errors.add(:application_date, "cannot be a future event")
+      return false
+    end
+  end
 
 private
 
-def text_style
-  self.description = self.description.downcase.titleize
-  self.location = self.location.downcase.titleize 
-  self.posting_url = self.posting_url.downcase  if self.posting_url
-  self.status = self.status.downcase.capitalize if self.status
-end
+  def text_style
+    self.description = self.description.downcase.titleize
+    self.location = self.location.downcase.titleize 
+    self.posting_url = self.posting_url.downcase  if self.posting_url
+    self.status = self.status.downcase.capitalize if self.status
+  end
 
 end
