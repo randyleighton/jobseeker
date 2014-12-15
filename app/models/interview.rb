c7da72d5a4e9eea3e7fdcb58e6d211b790672901
Randy Leighton <randyleighton@yahoo.com>
2014-12-15 00:52:48 -0800

    interview model validation for interview time and test passing

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,5 +4,6 @@ class Interview < ActiveRecord::Base
   has_one :company, through: :job
 
   validates :interview_date, presence: true
+  validates :interview_time, presence: true
 
 end
--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -2,6 +2,8 @@ describe Interview do
 
   it { should belong_to :job } 
   it { should validate_presence_of :interview_date }
+  it { should validate_presence_of :interview_time }
+
   it { should have_one(:company).through(:job)}
 
   it "should have a contact id" do
