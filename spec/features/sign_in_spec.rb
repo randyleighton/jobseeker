f9441c16c2552b43047c985b4f46e43ded94ecba
Randy Leighton <randyleighton@yahoo.com>
2014-12-15 18:21:19 -0800

    capybara user sign up will allow the registered user to sign in - passing

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,16 +1,15 @@
 describe "user sign up", type: :feature do
   
   scenario "will successfully allow a user to sign up" do
-    binding.pry
     user = FactoryGirl.create(:user)
-    register_user(user)
+    register_user(user.email, user.username, user.password, user.password)
     expect(page).to have_content 'Logged in as'
   end
 
   scenario "will allow the registered user to sign in" do
     user = FactoryGirl.create(:user)
-    sign_in_user(user)
-    expect(page).to have_content 'Logged in as'
+    sign_in_user(user.email, user.password)
+    expect(page).to have_content 'Signed in successfully.'
   end
 
 end
