fda9a228310d2c5a373901627c5c8c9fd91eccb2
Randy Leighton <randyleighton@yahoo.com>
2014-12-15 18:59:53 -0800

    Capybara view navigation links when logged in test pass

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -8,6 +8,19 @@ feature 'Navigation links', type: :feature do
     expect(page).to have_content 'Sign Up'
     expect(page).to have_content 'Log In'
     expect(page).to have_content 'Home'
-    save_and_open_page
   end
+
+  scenario 'view navigation links when logged in' do
+    user = FactoryGirl.create(:user)
+    visit new_user_registration_path
+    sign_in_user(user.email, user.password)
+    visit root_path
+    expect(page).to have_content 'Companies'
+    expect(page).to have_content 'Contacts'
+    expect(page).to have_content 'Jobs'
+    expect(page).to have_content 'Interviews'
+    expect(page).to have_content 'Edit Profile'
+    expect(page).to have_content 'Logout'
+  end
+
 end
