75821644de22dfd1c4da7cbf74a8aeb4ce751688
Randy Leighton <randyleighton@yahoo.com>
2014-12-21 12:14:35 -0800

    reminder validates presence of due date

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -2,6 +2,7 @@ class Reminder < ActiveRecord::Base
 
   belongs_to :rem, polymorphic: true
   validates :subject, presence: true
+  validates :due_date, presence: true
 
 
 end
--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -2,5 +2,7 @@ describe Reminder do
 
 it { should belong_to :rem}
 it { should validate_presence_of :subject }
+it { should validate_presence_of :due_date }
+
 
 end
