d12a6507957a8177d302028b08e7d95869beb33a
Randy Leighton <randyleighton@yahoo.com>
2014-12-21 12:22:17 -0800

    update response tests to passing

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -4,15 +4,17 @@ describe Response do
   it { should have_one(:company).through(:job) }
   it { should validate_presence_of :response_date }
 
+let!(:job) { FactoryGirl.create(:job, application_date:DateTime.now-5)}
+
   it "should have a contact id" do
     contact = FactoryGirl.create(:contact)
-    response = FactoryGirl.create(:response, contact_id: contact.id)
+    response = FactoryGirl.create(:response, contact_id: contact.id, job_id: job.id, response_date: DateTime.now)
     expect(Response.all).to eq [response]
   end
 
   it "should be unique to a user" do
     user = FactoryGirl.create(:user)
-    response = FactoryGirl.create(:response, user_id: user.id)
+    response = FactoryGirl.create(:response, user_id: user.id, job_id: job.id, response_date: DateTime.now)
     expect(response.user_id).to eq user.id
   end
 
