151f86279be96965d79d710250502fb3dbc8ebed
Randy Leighton <randyleighton@yahoo.com>
2015-01-01 15:43:54 -0800

    update forms and route for feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,9 +2,23 @@ class FeedbacksController < ApplicationController
   before_filter :authenticate_user!
 
   def new
+    @feedback = Feedback.new
   end
 
   def create
+    @feedback = Feedback.create(feedback_params)
+    if @feedback.valid?
+      redirect_to root_path, notice: "Thank you so much for the feedback, we will look at it"
+    else
+      render 'new'
+    end
+
+  end
+
+private
+
+  def feedback_params
+    params.require(:feedback).permit(:user_id, :subject, :body)
   end
 
 end
