3bc14b09ec4411796064dfdb01a272bb5bceaf61
Randy Leighton <randyleighton@yahoo.com>
2015-01-01 22:19:39 -0800

    followup validates action only, other items are optional, test pass

--- app/models/followup.rb
+++ app/models/followup.rb
@@ -1,5 +1,8 @@
 class Followup < ActiveRecord::Base
 
   belongs_to :follow, polymorphic: true
+  validates :action, presence: true
+
+  scope :recent, ->(max){ limit(max) }
 
 end
--- spec/models/followup_spec.rb
+++ spec/models/followup_spec.rb
@@ -1,5 +1,7 @@
 describe Followup do
 
 it { should belong_to :follow }
+it { should validate_presence_of :action }
+
 
 end
