4af6abef2d9fbc7558346db739df7272f4266e14
Randy Leighton <randyleighton@yahoo.com>
2015-01-03 13:35:58 -0800

    company show more info button blur effect to remove focus

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
   $('#view-comp-info-btn').click(function() {
     $('#comp-info').toggle();
     $('#iconChange').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    $('#view-comp-info-btn').blur();
   });
 
 
