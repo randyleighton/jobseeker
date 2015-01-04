7536ec0e4145cad4ed039d5efb369273c3cd5417
Randy Leighton <randyleighton@yahoo.com>
2015-01-04 00:05:14 -0800

    create test for order by for followups

--- spec/models/followup_spec.rb
+++ spec/models/followup_spec.rb
@@ -9,5 +9,11 @@ let!(:company) { FactoryGirl.create(:company, name:"Beta", url: "www.beta.com") 
     followup = FactoryGirl.create(:followup, follow_id: company.id)
     expect(Followup.all).to eq [followup]
   end
+  it "should display followups in newest first order on index page" do
+    followup = FactoryGirl.create(:followup, follow_id: company.id, action: "Oldest Action")
+    followup2 = FactoryGirl.create(:followup, follow_id: company.id,action:"Newest Action")
+    expect(Followup.all.order_by).to eq [followup2, followup]
+  end
+
 
 end
