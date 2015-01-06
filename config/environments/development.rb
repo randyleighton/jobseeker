14a240baf3efe308c7379690c3fa7e1c9966393a
Randy Leighton <randyleighton@yahoo.com>
2015-01-05 22:04:55 -0800

    rails mail preview works when i changed config.action_mailer.delivery_method = :file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # mailer settings
   config.action_mailer.raise_delivery_errors = true
   # mailer setup for mailgun
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :file
   #added for devise
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
