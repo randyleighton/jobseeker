ecbde9074f7f39b6c0089472a75d0f044b5b098b
Randy Leighton <randyleighton@yahoo.com>
2015-01-05 22:17:09 -0800

    create preview for admin mailer feedback_confirmation

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -6,6 +6,10 @@ class AdminMailerPreview < ActionMailer::Preview
     AdminMailer.send_feedback(feedback).deliver_now
   end
 
+  def feedback_confirmation
+    feedback = Feedback.first
+    AdminMailer.feedback_confirmation(feedback).deliver_now
+  end
 
 end
 
