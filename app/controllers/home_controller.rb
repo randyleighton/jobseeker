f8fc1a6bc5915dda7ed326ce44b2014a37cb72b5
Randy Leighton <randyleighton@yahoo.com>
2015-01-13 21:45:31 -0800

    refactor home index in case to remove [] and replace with .none

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,10 @@ def index
     @jobs = Job.where(user_id:current_user.id).order_by.recent(10).by_status("Open")
     @reminders = Reminder.where(user_id: current_user.id).recent(10)
   else
-    @companies = []
-    @interviews = []
-    @jobs = []
-    @reminders = []
+    @companies = Company.none
+    @interviews = Interview.none
+    @jobs = Job.none
+    @reminders = Reminder.none
   end
 
 end
