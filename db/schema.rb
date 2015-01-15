af3c1be0ae23c8af505834b678a0386cfa1aac18
Randy Leighton <randyleighton@yahoo.com>
2015-01-15 15:41:55 -0800

    create accounts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109222334) do
+ActiveRecord::Schema.define(version: 20150115233618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "type"
+    t.string "info"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
