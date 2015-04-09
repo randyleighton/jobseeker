0b1fee91ccfcad3e9af574ad4da9aee5457de31c
Randy Leighton <randyleighton@yahoo.com>
2015-04-09 06:40:50 -0700

    require csv in app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,15 @@ module ApplicationHelper
 
   def datadump(current_user)
     companies = Company.where(user_id: current_user.id)
+    contacts = Contact.where(user_id: current_user.id)
+    jobs = Job.where(user_id: current_user.id)
+    interviews = Interview.where(user_id: current_user.id)
+    responses = Response.where(user_id: current_user.id)
+    followups = Followup.where(user_id: current_user.id)
+    feedbacks = Feedback.where(user_id: current_user.id)
+
+    
+
   end
 
 end
